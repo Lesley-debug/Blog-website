f3a15ff8ca904f354c797aaf73f9a1c1da8b568b
lesley tabi <esanglesley@gmail.com>
2025-07-26 10:02:26 +0100

    linked contact.php wit the main css file

--- contact.php
+++ contact.php
@@ -6,7 +6,11 @@
   <!DOCTYPE html>
 <html lang="en">
 <head>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+    <link rel="stylesheet" href="Portfolio_blog/css/style.css">
+    
 </head>
 <body>
       <h2>Contact me through email</h2>
